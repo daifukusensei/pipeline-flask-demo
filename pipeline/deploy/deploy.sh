1df23302f60cd79d2272f8186a17f4b629e83386
daifukusensei <chris.bandera@gmail.com>
2021-07-10 20:05:34 +0000

    Updated FQDN of EC2 instance

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -10,7 +10,7 @@ echo daifukusensei/flask-demo > /tmp/.auth
 echo $BUILD_NUMBER >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /var/jenkins_home/.ssh/jenkins /tmp/.auth ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:/tmp/.auth
-scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/build/docker-compose.yml ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:~
-scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/deploy/publish.sh ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:/tmp/publish.sh
-ssh -i /var/jenkins_home/.ssh/jenkins ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com "/tmp/publish.sh"
+scp -i /var/jenkins_home/.ssh/jenkins /tmp/.auth ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:/tmp/.auth
+scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/build/docker-compose.yml ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:~
+scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/deploy/publish.sh ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:/tmp/publish.sh
+ssh -i /var/jenkins_home/.ssh/jenkins ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com "/tmp/publish.sh"
