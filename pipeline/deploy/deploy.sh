a1c2cf6bbac55d150bd1605221df7853a17290a4
daifukusensei <chris.bandera@gmail.com>
2021-07-10 19:46:46 +0000

    Refactor

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -4,12 +4,13 @@ echo "**************************************"
 echo "** Deploying Docker image to target **"
 echo "**************************************"
 
-cd pipeline/build/ && docker-compose up -d
+#cd pipeline/build/ && docker-compose up -d
 
-#echo flask-demo > /tmp/.auth
-#echo $BUILD_TAG >> /tmp/.auth
-#echo $PASS >> /tmp/.auth
+echo daifukusensei/flask-demo > /tmp/.auth
+echo $BUILD_NUMBER >> /tmp/.auth
+echo $PASS >> /tmp/.auth
 
-#scp /tmp/.auth vagrant@192.168.100.101:/tmp/.auth
-#scp ./pipeline/deploy/publish.sh vagrant@192.168.100.101:/tmp/publish.sh
-#ssh vagrant@192.168.100.101 "/tmp/publish.sh"
+scp /tmp/.auth ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:/tmp/.auth
+scp ./pipeline/build/docker-compose.yml ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:~
+scp ./pipeline/deploy/publish.sh ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com:/tmp/publish.sh
+ssh ec2-user@ec2-3-215-179-24.compute-1.amazonaws.com "/tmp/publish.sh"
--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -6,11 +6,11 @@ echo "**************************************"
 
 # export variables
 export IMAGE=$(sed -n '1p' /tmp/.auth)
-export BUILD_TAG=$(sed -n '2p' /tmp/.auth)
+export BUILD_NUMBER=$(sed -n '2p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
 
 # login to docker hub
 docker login -u daifukusensei -p $PASS
 
 # pull docker image and docker-compose start
-cd ~/maven && docker-compose up -d
+cd ~ && docker pull $IMAGE:$BUILD_NUMBER && docker-compose up -d
