9629b842eb85a5528c88ea755b60c91b47b25e80
daifukusensei <chris.bandera@gmail.com>
2021-07-10 20:31:22 +0000

    Remove credentials from deploy.sh

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -8,9 +8,9 @@ echo "**************************************"
 
 echo daifukusensei/flask-demo > /tmp/.auth
 echo $BUILD_NUMBER >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+#echo $PASS >> /tmp/.auth
 
-scp -i /var/jenkins_home/.ssh/jenkins /tmp/.auth ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:/tmp/.auth
-scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/build/docker-compose.yml ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:~
-scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/deploy/publish.sh ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com:/tmp/publish.sh
-ssh -i /var/jenkins_home/.ssh/jenkins ec2-user@ec2-3-91-25-15.compute-1.amazonaws.com "/tmp/publish.sh"
+scp -i /var/jenkins_home/.ssh/jenkins /tmp/.auth ec2-user@ec2-3-238-70-144.compute-1.amazonaws.com:/tmp/.auth
+scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/build/docker-compose.yml ec2-user@ec2-3-238-70-144.compute-1.amazonaws.com:~
+scp -i /var/jenkins_home/.ssh/jenkins ./pipeline/deploy/publish.sh ec2-user@ec2-3-238-70-144.compute-1.amazonaws.com:/tmp/publish.sh
+ssh -i /var/jenkins_home/.ssh/jenkins ec2-user@ec2-3-238-70-144.compute-1.amazonaws.com "/tmp/publish.sh"
--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -7,10 +7,10 @@ echo "**************************************"
 # export variables
 export IMAGE=$(sed -n '1p' /tmp/.auth)
 export BUILD_NUMBER=$(sed -n '2p' /tmp/.auth)
-export PASS=$(sed -n '3p' /tmp/.auth)
+#export PASS=$(sed -n '3p' /tmp/.auth)
 
 # login to docker hub
-docker login -u daifukusensei -p $PASS
+#docker login -u daifukusensei -p $PASS
 
 # pull docker image and docker-compose start
 cd ~ && docker pull $IMAGE:$BUILD_NUMBER && docker-compose up -d
