5034c9a3e7afed17750c843ce744687787522c0a
daifukusensei <chris.bandera@gmail.com>
2021-07-10 17:17:09 +0000

    Updated test.sh

--- pipeline/test/test.sh
+++ pipeline/test/test.sh
@@ -4,4 +4,6 @@ echo "**************************************"
 echo "********** Testing new code **********"
 echo "**************************************"
 
+docker-compose -f src/docker-compose.yml up -d
 docker exec -i web python < pipeline/test/test.py
+docker-compose -f src/docker-compose.yml stop
