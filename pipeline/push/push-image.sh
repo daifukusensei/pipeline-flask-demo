7f4158f449321c68620c97d5af6ad58224b42013
daifukusensei <chris.bandera@gmail.com>
2021-07-11 02:25:59 +0000

    Use IMAGE variable defined in Jenkinsfile

--- pipeline/push/push-image.sh
+++ pipeline/push/push-image.sh
@@ -4,14 +4,8 @@ echo "**************************************"
 echo "* Pushing Docker image to Docker Hub *"
 echo "**************************************"
 
-#IMAGE="flask-demo"
-
 echo "Logging in..."
 docker login -u daifukusensei -p $PASS
 
-echo "Tagging image..."
-#docker tag $IMAGE:$BUILD_NUMBER daifukusensei/$IMAGE:$BUILD_NUMBER
-
 echo "Pushing image..."
-#docker push daifukusensei/$IMAGE:$BUILD_NUMBER
 docker push $IMAGE:$BUILD_NUMBER
