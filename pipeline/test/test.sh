85383b77009429863293e07cbb280a38d37ef978
daifukusensei <chris.bandera@gmail.com>
2021-07-12 16:06:05 +0000

    Copy test results from container

--- pipeline/test/test.sh
+++ pipeline/test/test.sh
@@ -4,8 +4,18 @@ echo "**************************************"
 echo "********** Testing new code **********"
 echo "**************************************"
 
+# launch containers
 docker-compose -f src/docker-compose.yml up -d
+
+# run tests and get return code
 docker exec -i web python < pipeline/test/test.py
 RCODE=$?
+
+# copy test results from container to build server
+docker cp web:test-reports/* pipeline/test/test-reports/
+
+# bring down containers
 docker-compose -f src/docker-compose.yml down
+
+# exit with return code from completed tests
 exit $RCODE
