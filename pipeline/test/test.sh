8ef5e88c9203f2e02e4a8c081b4bbc1b44d466c0
daifukusensei <chris.bandera@gmail.com>
2021-07-12 16:15:10 +0000

    Copy test results from container

--- pipeline/test/test.sh
+++ pipeline/test/test.sh
@@ -12,7 +12,7 @@ docker exec -i web python < pipeline/test/test.py
 RCODE=$?
 
 # copy test results from container to build server
-docker cp web:test-reports/* pipeline/test/test-reports/
+docker cp web:/code/test-reports pipeline/test/
 
 # bring down containers
 docker-compose -f src/docker-compose.yml down
