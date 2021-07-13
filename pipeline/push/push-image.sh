8f407765184453841581b2779cfc04dc9611e21e
daifukusensei <chris.bandera@gmail.com>
2021-07-13 20:42:26 +0000

    Build Docker images based on build number

--- pipeline/push/push-image.sh
+++ pipeline/push/push-image.sh
@@ -8,8 +8,7 @@ echo "Logging in to Docker Hub..."
 docker login -u daifukusensei -p $PASS
 
 echo "Pushing image..."
-#docker push $IMAGE:$BUILD_NUMBER
-docker push $IMAGE:latest
+docker push $IMAGE:$BUILD_NUMBER
 
 echo "Removing image(s) from build server..."
 docker-compose -f src/docker-compose.yml down --rmi all
