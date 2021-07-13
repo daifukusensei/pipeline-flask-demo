f63bcfa2b0a4f9baa6992725cff46558344b2986
daifukusensei <chris.bandera@gmail.com>
2021-07-13 19:45:58 +0000

    Delete older images in Deploy stage

--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -10,7 +10,7 @@ export BUILD_NUMBER=$(sed -n '2p' /tmp/.auth)
 
 echo "Building and starting Docker containers on target host..."
 docker pull $IMAGE:$BUILD_NUMBER && docker-compose up -d
-if [ #? -eq 0 ]; then
+if [ $? -eq 0 ]; then
   echo "Deleting older $IMAGE images..."
   docker rmi $(docker images | grep 'daifukusensei/flask-demo')
 fi
