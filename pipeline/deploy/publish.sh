22e21e95bf9c859e063b639719ac29077a81561b
daifukusensei <chris.bandera@gmail.com>
2021-07-13 20:10:19 +0000

    Fixed reading of .vars file in Deploy stage

--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -5,8 +5,8 @@ echo "** Publishing Docker image on target *"
 echo "**************************************"
 
 # export variables
-export IMAGE=$(sed -n '1p' /tmp/.auth)
-export BUILD_NUMBER=$(sed -n '2p' /tmp/.auth)
+export IMAGE=$(sed -n '1p' .vars)
+export BUILD_NUMBER=$(sed -n '2p' .vars)
 
 echo "Building and starting Docker containers on target host..."
 docker pull $IMAGE:$BUILD_NUMBER && docker-compose up -d
