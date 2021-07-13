3d917a13e5f11a58bcc97f72a666efb4c61532f0
daifukusensei <chris.bandera@gmail.com>
2021-07-13 13:42:42 +0000

    Updated comments in Test and Push stages

--- pipeline/push/push-image.sh
+++ pipeline/push/push-image.sh
@@ -4,8 +4,11 @@ echo "**************************************"
 echo "* Pushing Docker image to Docker Hub *"
 echo "**************************************"
 
-echo "Logging in..."
+echo "Logging in to Docker Hub..."
 docker login -u daifukusensei -p $PASS
 
 echo "Pushing image..."
 docker push $IMAGE:$BUILD_NUMBER
+
+echo "Removing image(s) from build server..."
+docker-compose -f srcdocker-R
--- pipeline/test/test.sh
+++ pipeline/test/test.sh
@@ -4,17 +4,18 @@ echo "**************************************"
 echo "********** Testing new code **********"
 echo "**************************************"
 
-# launch containers
+echo "Building and launching containers on build server..."
+pwd
 docker-compose -f src/docker-compose.yml up -d
 
-# run tests and get return code
+echo "Running tests in container..."
 docker exec -i web python < pipeline/test/test.py
 RCODE=$?
 
-# copy test results from container to build server
+echo "Copy test results from container to build server..."
 docker cp web:/code/test-reports pipeline/test/
 
-# bring down containers
+echo "Stopping containers..."
 docker-compose -f src/docker-compose.yml down
 
 # exit with return code from completed tests
