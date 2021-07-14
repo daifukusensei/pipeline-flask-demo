6e49674eccad4b640c5f010b3aa66c32322e3bff
daifukusensei <chris.bandera@gmail.com>
2021-07-14 01:29:28 +0000

    Reverted Redis port to 6379 and updated app.py; test.py

--- src/app.py
+++ src/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6389)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello again, World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
