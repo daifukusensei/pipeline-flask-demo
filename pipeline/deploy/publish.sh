f578574f69156ee7dc26b1cdf43f8a2df2fb0680
daifukusensei <chris.bandera@gmail.com>
2021-07-14 02:10:09 +0000

    Updated tag to Build-BUILD_NUMBER

--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -9,9 +9,9 @@ export IMAGE=$(sed -n '1p' .vars)
 export BUILD_NUMBER=$(sed -n '2p' .vars)
 
 echo "Building and starting Docker containers on target host..."
-docker pull $IMAGE:$BUILD_NUMBER && docker-compose up -d
+docker pull $IMAGE:Build-$BUILD_NUMBER && docker-compose up -d
 
 if [ $? -eq 0 ]; then
    echo "Deleting older $IMAGE images from target host..."
-   docker rmi $(docker images | grep '$IMAGE' | grep -E -v '$IMAGE.*$BUILD_NUMBER' | awk '{ print $3 }')
+   docker rmi $(docker images | grep '$IMAGE' | grep -E -v '$IMAGE.*Build-$BUILD_NUMBER' | awk '{ print $3 }')
 fi
